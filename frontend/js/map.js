220b176539de7bde86aef004c9ce8d53054a1122
Meerab <meerab.xox005@gmail.com>
2026-07-29 00:44:52 +0400

    Add Leaflet live map

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -1,22 +1,69 @@
+let map;
+let marker;
+window.onload = function () {
+
+    console.log("Map initialized");
+
+    map = L.map('map').setView([0, 0], 2);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+};
+
+window.onload = function () {
+
+    map = L.map('map').setView([0, 0], 2);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+        attribution: '&copy; OpenStreetMap contributors'
+
+    }).addTo(map);
+
+};
+
 function getLocation() {
 
-    if (navigator.geolocation) {
-
-        navigator.geolocation.getCurrentPosition(showPosition);
-
-    } else {
+    if (!navigator.geolocation) {
 
         alert("Geolocation is not supported.");
 
+        return;
+
     }
 
-}
+    navigator.geolocation.getCurrentPosition(
 
-function showPosition(position) {
+        function(position) {
 
-    alert(
-        "Latitude: " + position.coords.latitude +
-        "\nLongitude: " + position.coords.longitude
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
+
+            map.setView([lat, lng], 16);
+
+            if (marker) {
+
+                map.removeLayer(marker);
+
+            }
+
+            marker = L.marker([lat, lng]).addTo(map);
+
+            marker.bindPopup("📍 You are here").openPopup();
+
+            document.getElementById("locationText").innerHTML =
+
+                `Latitude: ${lat}<br>Longitude: ${lng}`;
+
+        },
+
+        function(error) {
+
+            alert("Unable to retrieve your location.");
+
+        }
+
     );
 
 }
--- frontend/js/services.js
+++ frontend/js/services.js
@@ -1,1 +1,27 @@
-console.log("Nearby Services Loaded");
+function openNearby(place) {
+
+    if (!navigator.geolocation) {
+        alert("Geolocation is not supported.");
+        return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+
+        function(position) {
+
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
+
+            const url =
+                `https://www.google.com/maps/search/${encodeURIComponent(place)}/@${lat},${lng},15z`;
+
+            window.open(url, "_blank");
+
+        },
+
+        function() {
+            alert("Unable to get your location.");
+        }
+
+    );
+}
