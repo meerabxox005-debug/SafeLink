9bc75206fc31222d29cabd4ecfc18da9da14b080
Meerab <meerab.xox005@gmail.com>
2026-07-28 19:08:52 +0400

    Configure PWA service worker

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,7 +1,31 @@
+const CACHE_NAME = "safelink-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/login.html",
+  "/signup.html",
+  "/dashboard.html",
+  "/profile.html",
+  "/contacts.html",
+  "/chatbot.html",
+  "/sos.html",
+  "/css/style.css",
+  "/js/config.js"
+];
+
 self.addEventListener("install", event => {
-    console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
