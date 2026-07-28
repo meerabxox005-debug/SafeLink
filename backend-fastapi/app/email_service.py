382a3b08fe400a9ab4ceb390356053a768c8d369
Meerab <meerab.xox005@gmail.com>
2026-07-28 16:36:44 +0400

    Switch email to Brevo API

--- backend-fastapi/app/email_service.py
+++ backend-fastapi/app/email_service.py
@@ -1,30 +1,34 @@
-from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
-from pydantic import EmailStr
-from dotenv import load_dotenv
 import os
+import requests
+from dotenv import load_dotenv
 
 load_dotenv()
 
-print("MAIL_SERVER:", os.getenv("MAIL_SERVER"))
-print("MAIL_PORT:", os.getenv("MAIL_PORT"))
-print("MAIL_USERNAME:", os.getenv("MAIL_USERNAME"))
-conf = ConnectionConfig(
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM=os.getenv("MAIL_FROM"),
-    MAIL_PORT=int(os.getenv("MAIL_PORT")),
-    MAIL_SERVER=os.getenv("MAIL_SERVER"),
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
-    USE_CREDENTIALS=True,
-)
+BREVO_API_KEY = os.getenv("BREVO_API_KEY")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+
 
 async def send_sos_email(receiver: str, user_email: str, latitude: float, longitude: float):
+    url = "https://api.brevo.com/v3/smtp/email"
 
-    message = MessageSchema(
-        subject="🚨 SafeLink Emergency Alert",
-        recipients=[receiver],
-        body=f"""
+    headers = {
+        "accept": "application/json",
+        "api-key": BREVO_API_KEY,
+        "content-type": "application/json"
+    }
+
+    data = {
+        "sender": {
+            "name": "SafeLink",
+            "email": SENDER_EMAIL
+        },
+        "to": [
+            {
+                "email": receiver
+            }
+        ],
+        "subject": "🚨 SafeLink Emergency Alert",
+        "textContent": f"""
 Emergency Alert!
 
 {user_email} has triggered an SOS.
@@ -33,9 +37,13 @@ Location:
 https://maps.google.com/?q={latitude},{longitude}
 
 Please contact them immediately.
-""",
-        subtype="plain"
-    )
+"""
+    }
 
-    fm = FastMail(conf)
-    await fm.send_message(message)
+    response = requests.post(url, headers=headers, json=data)
+
+    print("Brevo Status:", response.status_code)
+    print("Brevo Response:", response.text)
+
+    if response.status_code not in [200, 201]:
+        raise Exception(f"Brevo API Error: {response.text}")
